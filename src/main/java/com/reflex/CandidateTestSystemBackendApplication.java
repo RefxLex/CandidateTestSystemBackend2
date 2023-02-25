8e67510645411f21fa81a052e5ca0c4a383d9bf5
Ref#Lex <jediealex@gmail.com>
2023-02-25 13:25:07 +0300

    init commit

---
+++ src/main/java/com/reflex/CandidateTestSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.reflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateTestSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateTestSystemBackendApplication.class, args);
+	}
+
+}
