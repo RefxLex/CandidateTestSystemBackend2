f5090eae8baac592153aaf690777020bd63608d0
reflex <reflex@reflexCOMP>
2023-06-03 17:53:12 +0500

    linux testing and bugs fixed

--- src/main/java/com/reflex/controller/UserTaskController.java
+++ src/main/java/com/reflex/controller/UserTaskController.java
@@ -187,7 +187,7 @@ public class UserTaskController {
 		String fs = System.getProperty("file.separator");
     	boolean isWindows = System.getProperty("os.name").toLowerCase().startsWith("windows");
     	boolean isLinux = System.getProperty("os.name").toLowerCase().startsWith("linux");
-    	
+    	    	
     	Path path = Paths.get(System.getProperty("user.home") + fs + "cadidate_test_system_projects");
     	ProcessBuilder processBuilder = new ProcessBuilder();
     	
@@ -200,7 +200,7 @@ public class UserTaskController {
 	    				+ "lib");
 	    	} 
 	    	else if (isLinux){
-	    		processBuilder.command("sh", "-c", "mkdir cadidate_test_system_projects" + fs + "test_launch" + " " + "cadidate_test_system_projects" + fs
+	    		processBuilder.command("sh", "-c", "mkdir -p cadidate_test_system_projects" + fs + "test_launch" + " " + "cadidate_test_system_projects" + fs
 	    				+ "lib");
 	    	}
 	    	runProcess(processBuilder);
@@ -208,11 +208,11 @@ public class UserTaskController {
 	        // download JUnit 5 jars
 	    	processBuilder.directory(new File(System.getProperty("user.home")));
 	    	if (isWindows) {
-	    		processBuilder.command("cmd.exe", "/c", "mvn org.apache.maven.plugins:maven-dependency-plugin:3.6.0:get" +
+	    		processBuilder.command("cmd.exe", "/c", "/optmvn org.apache.maven.plugins:maven-dependency-plugin:3.6.0:get" +
 	    	" -DrepoUrl=https://download.java.net/maven/2/ -Dartifact=org.junit.jupiter:junit-jupiter-api:5.9.2");
 	    	} 
 	    	else if (isLinux){
-	    		processBuilder.command("sh", "-c", "mvn org.apache.maven.plugins:maven-dependency-plugin:3.6.0:get" +
+	    		processBuilder.command("sh", "-c", "apache-maven-3.8.3/bin/mvn org.apache.maven.plugins:maven-dependency-plugin:3.6.0:get" +
 	    		    	" -DrepoUrl=https://download.java.net/maven/2/ -Dartifact=org.junit.jupiter:junit-jupiter-api:5.9.2");
 	    	}
 	    	runProcess(processBuilder);
@@ -222,7 +222,7 @@ public class UserTaskController {
 	    	" -DrepoUrl=https://download.java.net/maven/2/ -Dartifact=org.junit.jupiter:junit-jupiter-api:5.9.2");
 	    	} 
 	    	else if (isLinux){
-	    		processBuilder.command("sh", "-c", "mvn org.apache.maven.plugins:maven-dependency-plugin:3.6.0:get" +
+	    		processBuilder.command("sh", "-c", "apache-maven-3.8.3/bin/mvn org.apache.maven.plugins:maven-dependency-plugin:3.6.0:get" +
 	    	" -DrepoUrl=https://download.java.net/maven/2/ -Dartifact=org.junit.platform:junit-platform-console-standalone:1.9.3");
 	    	}
 	    	runProcess(processBuilder);
@@ -317,7 +317,7 @@ public class UserTaskController {
 	    			+ "java_project" + projectKey + " " + projectKey + fs + "target" + fs + "classes" + " " + projectKey + fs + "target" + fs + "test_classes");
 	    } 
 	    else if (isLinux){
-	    	processBuilder.command("sh", "-c", "mkdir " + projectKey + fs + "src" + fs + "main" + fs + "java" + fs + "com" + fs + "cleverhire" + fs
+	    	processBuilder.command("sh", "-c", "mkdir -p " + projectKey + fs + "src" + fs + "main" + fs + "java" + fs + "com" + fs + "cleverhire" + fs
 	    			+ "java_project" + projectKey + " " + projectKey + fs + "src" + fs + "test" + fs + "java" + fs + "com" + fs + "cleverhire" + fs
 	    			+ "java_project" + projectKey + " " + projectKey + fs + "target" + fs + "classes" + " " + projectKey + fs + "target" + fs + "test_classes");
 	    }
@@ -389,7 +389,7 @@ public class UserTaskController {
 	    			+ "java_project" + projectKey + fs + "*.java");
 	    } 
 	    else if (isLinux){
-	    	processBuilder.command("sh", "-c", "javac -d target" + fs + "classes" + " src" + fs + "main" + fs + "java" + fs + "com" + fs + "cleverhire" + fs 
+	    	processBuilder.command("sh", "-c", "~/jdk-17.0.6/bin/javac -d target" + fs + "classes" + " src" + fs + "main" + fs + "java" + fs + "com" + fs + "cleverhire" + fs 
 	    			+ "java_project" + projectKey + fs + "*.java");
 	    }
 	    String mainCompileResult = runProcess(processBuilder);
@@ -416,6 +416,7 @@ public class UserTaskController {
 	    }
 	    
 	    // compile test
+	    System.out.println("/////////compile test");
 	    processBuilder.directory(new File(System.getProperty("user.home") + fs + "cadidate_test_system_projects" + fs + "test_launch" + fs + projectKey));
 	    if (isWindows) {
 	    	processBuilder.command("cmd.exe", "/c", "javac -d target" + fs + "test_classes" + " -cp %USERPROFILE%" + fs + "cadidate_test_system_projects" + fs 
@@ -423,8 +424,8 @@ public class UserTaskController {
 	    			+ projectKey + fs + "*.java");
 	    } 
 	    else if (isLinux){
-	    	processBuilder.command("sh", "-c", "javac -d target"+ fs + "classes" + fs + "test_classes" + " -cp ~" + fs + "cadidate_test_system_projects" + fs
-	    			+ "lib" + fs + "*;target" + " src" + fs + "test" + fs + "java" + fs + "com" + fs + "cleverhire" + fs + "java_project" + projectKey + fs + "*.java");
+	    	processBuilder.command("sh", "-c", "~/jdk-17.0.6/bin/javac -d target" + fs + "test_classes" + " -cp ~" + fs + "cadidate_test_system_projects" + fs
+	    			+ "lib" + fs + "*:target" + fs + "classes" + " src" + fs + "test" + fs + "java" + fs + "com" + fs + "cleverhire" + fs + "java_project" + projectKey + fs + "*.java");
 	    }
 	    String testCompileResult = runProcess(processBuilder);
 	    
@@ -436,7 +437,7 @@ public class UserTaskController {
 	        String encodedTestCompileResult = Base64.getEncoder().encodeToString(testCompileResult.getBytes(StandardCharsets.ISO_8859_1));
 	        response.put("result", encodedTestCompileResult);
 	        
-	  	  	// delete created folder
+	  	  	// delete created folder      
 		    processBuilder.directory(new File(System.getProperty("user.home") + fs + "cadidate_test_system_projects" + fs + "test_launch"));
 		    if (isWindows) {
 		    	processBuilder.command("cmd.exe", "/c", "rmdir /s /q " + projectKey);
@@ -458,8 +459,8 @@ public class UserTaskController {
 		    			+ " --select-class" + " com.cleverhire." + "java_project" + projectKey + "." + testClassName);
 		    } 
 		    else if (isLinux){
-		    	processBuilder.command("sh", "-c", "java -jar" + " ~" + fs + "cadidate_test_system_projects" + fs + "lib" + fs
-		    			+ "junit-platform-console-standalone-1.9.3.jar" + " --class-path" + " target"  + fs + "classes;target" + fs + "test_classes" 
+		    	processBuilder.command("sh", "-c", "~/jdk-17.0.6/bin/java -jar" + " ~" + fs + "cadidate_test_system_projects" + fs + "lib" + fs
+		    			+ "junit-platform-console-standalone-1.9.3.jar" + " --class-path" + " target"  + fs + "classes:target" + fs + "test_classes" 
 		    			+ " --select-class" + " com.cleverhire." + "java_project" + projectKey + "." + testClassName);
 		    }
 		    testsResult = testsResult + runProcess(processBuilder);
@@ -516,7 +517,7 @@ public class UserTaskController {
 		    			+ "java_project" + projectKey + " " + projectKey + fs + "target" + fs + "classes" + " " + projectKey + fs + "target" + fs + "test_classes");
 		    } 
 		    else if (isLinux){
-		    	processBuilder.command("sh", "-c", "mkdir " + projectKey + fs + "src" + fs + "main" + fs + "java" + fs + "com" + fs + "cleverhire" + fs
+		    	processBuilder.command("sh", "-c", "mkdir -p " + projectKey + fs + "src" + fs + "main" + fs + "java" + fs + "com" + fs + "cleverhire" + fs
 		    			+ "java_project" + projectKey + " " + projectKey + fs + "src" + fs + "test" + fs + "java" + fs + "com" + fs + "cleverhire" + fs
 		    			+ "java_project" + projectKey + " " + projectKey + fs + "target" + fs + "classes" + " " + projectKey + fs + "target" + fs + "test_classes");
 		    }
@@ -590,7 +591,7 @@ public class UserTaskController {
 		    			+ "java_project" + projectKey + fs + "*.java");
 		    } 
 		    else if (isLinux){
-		    	processBuilder.command("sh", "-c", "javac -d target" + fs + "classes" + " src" + fs + "main" + fs + "java" + fs + "com" + fs + "cleverhire" + fs 
+		    	processBuilder.command("sh", "-c", "~/jdk-17.0.6/bin/javac -d target" + fs + "classes" + " src" + fs + "main" + fs + "java" + fs + "com" + fs + "cleverhire" + fs 
 		    			+ "java_project" + projectKey + fs + "*.java");
 		    }
 		    String mainCompileResult = runProcess(processBuilder);
@@ -617,8 +618,8 @@ public class UserTaskController {
 		    			+ projectKey + fs + "*.java");
 		    } 
 		    else if (isLinux){
-		    	processBuilder.command("sh", "-c", "javac -d target"+ fs + "classes" + fs + "test_classes" + " -cp ~" + fs + "cadidate_test_system_projects" + fs
-		    			+ "lib" + fs + "*;target" + " src" + fs + "test" + fs + "java" + fs + "com" + fs + "cleverhire" + fs + "java_project" + projectKey + fs + "*.java");
+		    	processBuilder.command("sh", "-c", "~/jdk-17.0.6/bin/javac -d target" + fs + "test_classes" + " -cp ~" + fs + "cadidate_test_system_projects" + fs
+		    			+ "lib" + fs + "*:target" + fs + "classes" + " src" + fs + "test" + fs + "java" + fs + "com" + fs + "cleverhire" + fs + "java_project" + projectKey + fs + "*.java");
 		    }
 		    String testCompileResult = runProcess(processBuilder);
 		    
@@ -649,8 +650,8 @@ public class UserTaskController {
 				    			+ " --select-class" + " com.cleverhire." + "java_project" + projectKey + "." + testClassName);
 				    } 
 				    else if (isLinux){
-				    	processBuilder.command("sh", "-c", "java -jar" + " ~" + fs + "cadidate_test_system_projects" + fs + "lib" + fs
-				    			+ "junit-platform-console-standalone-1.9.3.jar" + " --class-path" + " target"  + fs + "classes;target" + fs + "test_classes" 
+				    	processBuilder.command("sh", "-c", "~/jdk-17.0.6/bin/java -jar" + " ~" + fs + "cadidate_test_system_projects" + fs + "lib" + fs
+				    			+ "junit-platform-console-standalone-1.9.3.jar" + " --class-path" + " target"  + fs + "classes:target" + fs + "test_classes" 
 				    			+ " --select-class" + " com.cleverhire." + "java_project" + projectKey + "." + testClassName);
 				    }
 				    testsResult = runProcess(processBuilder);
